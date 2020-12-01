52b49721d4deea8a602896686d78f6f2ff5f1e0f
Bob Wei <bob.p.wei@gmail.com>
2020-12-01 01:22:59 -0500

    Completed day 1.

---
+++ 01.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+#
+# thehungryturnip@gmail.com
+#
+# Advent of Code 2020: Day 01
+#
+
+import sys
+
+TARGET = 2020
+
+with open('01.in', 'r') as f:
+    expenses = [int(l) for l in f.readlines()]
+
+looking_for = set()
+for e in expenses:
+    e_other = TARGET - e
+    if e in looking_for:
+        print(f'[01a] The 2 entries are {e_other} * {e} = '
+              f'{e_other * e}.')
+    else:
+        looking_for.add(e_other)
+
+for i in range(0, len(expenses)):
+    looking_for = set()
+    target = TARGET - expenses[i]
+    for j in expenses[i:]:
+        e_other = target - j
+        if j in looking_for:
+            e_i = expenses[i]
+            print(f'[01b] The 3 entries are {e_i} * {e_other} * {j} = '
+                  f'{e_i * e_other * j}.')
+        else:
+            looking_for.add(e_other)
