cf72efa4b99646ae04461e47fd6cee5ba2692aff
The Hungry Turnip <thehungryturnip@gmail.com>
2020-12-03 15:41:11 -0500

    Completed day 3 part b.

--- 03.py
+++ 03.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python3
 
 import sys
+from collections import namedtuple
+from functools import reduce
+
+Slope = namedtuple('Slope', ['dr', 'dc'])
 
 class MapRow(list):
     def __getitem__(self, c):
         return super().__getitem__(c % len(self))
+
+def count_trees(m, s):
+    tree_count = r = c = 0
+    while r < len(m):
+        if m[r][c] == '#':
+            tree_count += 1
+        r += s.dr
+        c += s.dc
+    return tree_count
 
 m = []
 with open ('03.in', 'r') as f:
     for l in f.readlines():
         m.append(MapRow(l.strip()))
 
-tree_count = 0
-for r in range(1, len(m)):
-    if m[r][r * 3] == '#':
-        tree_count += 1
+trees = count_trees(m, Slope(1, 3))
+print(f'[03a] Number of trees along the path is {trees}.')
 
-print(f'[03a] Number of trees along the path is {tree_count}.')
+slopes = [Slope(1, 1), Slope(1, 3), Slope(1, 5), Slope(1, 7), Slope(2, 1)]
+trees = list(map(lambda s: count_trees(m, s), slopes))
+multiply = reduce(lambda t1, t2: t1 * t2, trees)
+print(f'[03b] The tree counts for slopes {slopes} are {trees} which multiply to {multiply}.')
