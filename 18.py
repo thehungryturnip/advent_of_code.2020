389132ae705b983d099f307a9cdd4b5221a21555
The Hungry Turnip <thehungryturnip@gmail.com>
2020-12-21 18:40:25 -0500

    Stylistic update to 18.py.

--- 18.py
+++ 18.py
@@ -3,7 +3,7 @@
 import re
 from operator import add, mul
 
-def add_multiply(e, add_before_mul):
+def add_mul(e, add_then_mul):
     SYMBOL_TO_FUNC = {
             '+': add,
             '*': mul,
@@ -12,29 +12,29 @@ def add_multiply(e, add_before_mul):
     if len(e) == 3:
         return SYMBOL_TO_FUNC[e[1]](int(e[0]), int(e[2]))
 
-    if add_before_mul and '+' in e:
+    if add_then_mul and '+' in e:
         add_at = e.index('+')
-        val = add_multiply(e[add_at - 1: add_at + 2], add_before_mul)
+        val = add_mul(e[add_at - 1: add_at + 2], add_then_mul)
         before = e[:add_at - 1]
         after = e[add_at + 2:]
         e = before + [str(val)] + after
-        return add_multiply(e, add_before_mul)
+        return add_mul(e, add_then_mul)
 
-    val = add_multiply(e[:3], add_before_mul)
-    return add_multiply([str(val)] + e[3:], add_before_mul)
+    val = add_mul(e[:3], add_then_mul)
+    return add_mul([str(val)] + e[3:], add_then_mul)
 
-def calc(e, add_before_mul=False):
+def calc(e, add_then_mul=False):
     pattern = re.compile('\(([^()]+)\)')
     match = pattern.search(e)
 
     while match:
-        val = add_multiply(match[1].split(' '), add_before_mul)
+        val = add_mul(match[1].split(' '), add_then_mul)
         before = e[:match.start(0)]
         after = e[match.end(0):]
-        e = before + str(add_multiply(match[1].split(' '), add_before_mul)) + after
+        e = before + str(add_mul(match[1].split(' '), add_then_mul)) + after
         match = pattern.search(e)
 
-    return add_multiply(e.split(' '), add_before_mul)
+    return add_mul(e.split(' '), add_then_mul)
 
 with open('18.in', 'r') as f:
     expr = [l.strip() for l in f.readlines()]
